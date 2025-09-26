ba5136b54c73bdd335d74e51c037a27f0d81be79
cmowrer13 <130422485+cmowrer13@users.noreply.github.com>
2025-09-25 20:55:28 -0400

    Add better UI to start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,49 @@
-docker build . -t bios611project
-
-docker run -d \
--v "$(pwd -W)":/home/rstudio/work \
--v ${HOME}/.ssh:/home/rstudio/.ssh \
--v ${HOME}/.gitconfig:/home/rstudio/.gitconfig \
--e PASSWORD=611project \
--p 8787:8787 \
---name bios611project_container \
-bios611project 
+set -e
 
 BOLD="\033[1m"
 RESET="\033[0m"
 BLUE="\033[38;2;75;156;211m"
+RED="\033[38;2;220;50;47m"
+YELLOW="\033[38;2;215;215;85m"
+
+CONTAINER_NAME="bios611project_container"
+IMAGE_NAME="bios611project"
+
+if docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo -e "${YELLOW}Container '${CONTAINER_NAME}' is already running.${RESET}"
+    echo "Open http://localhost:8787 in your browser."
+    exit 0
+elif docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo -e "${YELLOW}Container '${CONTAINER_NAME}' exists but is stopped.${RESET}"
+    echo "Starting it now..."
+    docker start "${CONTAINER_NAME}" >/dev/null 2>&1
+    echo -e "${BOLD}Container started!${RESET} Open http://localhost:8787 in your browser."
+    echo -e "Username: ${BLUE}rstudio${RESET}"
+    echo -e "Password: ${BLUE}611project${RESET}"
+    exit 0
+fi
+
+if ! docker build . -t "${IMAGE_NAME}"; then
+    echo -e "${RED}Docker build failed. Exiting.${RESET}"
+    exit 1
+fi
+
+if ! docker run -d \
+    -v "$(pwd -W)":/home/rstudio/work \
+    -v "${HOME}/.ssh":/home/rstudio/.ssh \
+    -v "${HOME}/.gitconfig":/home/rstudio/.gitconfig \
+    -e PASSWORD=611project \
+    -p 8787:8787 \
+    --name "${CONTAINER_NAME}" \
+    "${IMAGE_NAME}"; then
+    echo -e "${RED}Docker run failed. Exiting.${RESET}"
+    exit 1
+fi
 
 echo ""
-echo -e "${BOLD}Container started!${RESET} Open localhost:8787 in your browser."
+echo -e "${BOLD}Container started!${RESET} Open http://localhost:8787 in your browser."
+echo ""
+
+echo -e "Work directory mounted at: ${BLUE}$(pwd -W)${RESET}"
 echo -e "Username: ${BLUE}rstudio${RESET}"
 echo -e "Password: ${BLUE}611project${RESET}"
