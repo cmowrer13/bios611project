c28f0c71be188a9df49fb4202e77f10acb1b16ba
cmowrer13 <130422485+cmowrer13@users.noreply.github.com>
2025-10-29 14:34:23 -0400

    Pull player IDs using baseballr

---
+++ pull_player_ids.R
@@ -0,0 +1,11 @@
+library(baseballr)
+
+player_ids <- chadwick_player_lu() %>% 
+  select(key_mlbam, name_last, name_first) %>% 
+  filter(!is.na(key_mlbam))
+
+players <- player_ids %>% 
+  mutate(player_name = paste(name_first, name_last)) %>% 
+  select(key_mlbam, player_name)
+
+write.csv(players, "raw_data/player_ids")
