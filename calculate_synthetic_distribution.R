e4072a7bfb82dca70bdb4f059378c5ec5aea7f30
cmowrer13 <130422485+cmowrer13@users.noreply.github.com>
2025-10-29 20:21:59 -0400

    Create script to calculate synthetic distributions

---
+++ calculate_synthetic_distribution.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+source("contact_distribution_function.R")
+
+similar_pitches <- read_csv("derived_data/similar_pitches.csv")
+similar_batters <- read_csv("derived_data/similar_batters.csv")
+
+normalize_weights <- function(df, dist_col = "distance"){
+  df %>% 
+    mutate(weight_raw = 1 / (!!sym(dist_col) + 1e-6),
+           weight = weight_raw / sum(weight_raw))
+}
+
+get_synthetic_distribution <- function(statcast_data,
+                                       batter_name,
+                                       pitcher_name,
+                                       pitch_type,
+                                       width = 5) {
+  
+  # 1. True matchup
+  dist_true <- get_contact_distribution(statcast_data, batter_name, pitcher_name, pitch_type, width)
+  n <- sum(dist_true$n, na.rm = TRUE)
+  
+  # 2. Similar pitches
+  sim_p <- similar_pitches %>%
+    filter(pitcher_name == !!pitcher_name, pitch_name == !!pitch_type) %>%
+    normalize_weights("distance")
+  
+  pitch_dists <- sim_p %>%
+    mutate(
+      dist = map2(neighbor_pitcher_name, neighbor_pitch_name,
+                  ~ get_contact_distribution(statcast_data, batter_name, .x, .y, width))
+    ) %>%
+    filter(!map_lgl(dist, is.null)) %>%
+    mutate(n_contrib = map_dbl(dist, ~ sum(.x$n, na.rm = TRUE))) %>%
+    mutate(weighted_dist = map2(dist, weight, ~ mutate(.x, freq = freq * .y)))
+  
+  if (nrow(pitch_dists) > 0) {
+    dist_pitch <- bind_rows(pitch_dists$weighted_dist) %>%
+      group_by(la_bin, ev_bin) %>%
+      summarise(freq = sum(freq, na.rm = TRUE), .groups = "drop")
+  } else {
+    dist_pitch <- dist_true %>% mutate(freq = 0)
+  }
+  
+  n_p <- sum((pitch_dists$weight^2) * pitch_dists$n_contrib, na.rm = TRUE)
+  
+  # 3. Similar batters
+  sim_b <- similar_batters %>%
+    filter(batter_name == !!batter_name, pitch_name == !!pitch_type) %>%
+    normalize_weights("distance")
+  
+  batter_dists <- sim_b %>%
+    mutate(
+      dist = map(neighbor_batter_name,
+                 ~ get_contact_distribution(statcast_data, .x, pitcher_name, pitch_type, width))
+    ) %>%
+    filter(!map_lgl(dist, is.null)) %>%
+    mutate(n_contrib = map_dbl(dist, ~ sum(.x$n, na.rm = TRUE))) %>%
+    mutate(weighted_dist = map2(dist, weight, ~ mutate(.x, freq = freq * .y)))
+  
+  if (nrow(batter_dists) > 0) {
+    dist_batter <- bind_rows(batter_dists$weighted_dist) %>%
+      group_by(la_bin, ev_bin) %>%
+      summarise(freq = sum(freq, na.rm = TRUE), .groups = "drop")
+  } else {
+    dist_batter <- dist_true %>% mutate(freq = 0)
+  }
+  
+  n_b <- sum((batter_dists$weight^2) * batter_dists$n_contrib, na.rm = TRUE)
+  
+  # 4. Combine weighted distributions
+  denom <- sqrt(n) + sqrt(n_p) + sqrt(n_b)
+  w_true <- sqrt(n) / denom
+  w_pitch <- sqrt(n_p) / denom
+  w_batter <- sqrt(n_b) / denom
+  
+  synthetic <- full_join(dist_true, dist_pitch, by = c("la_bin", "ev_bin"), suffix = c("", ".p")) %>%
+    full_join(dist_batter, by = c("la_bin", "ev_bin"), suffix = c("", ".b")) %>%
+    mutate(across(starts_with("freq"), replace_na, 0)) %>%
+    mutate(freq = w_true * freq + w_pitch * freq.p + w_batter * freq.b) %>%
+    transmute(la_bin, ev_bin, freq = freq / sum(freq, na.rm = TRUE))
+  
+  list(
+    synthetic_distribution = synthetic,
+    weights = c(real = w_true, pitch = w_pitch, batter = w_batter)
+  )
+}
